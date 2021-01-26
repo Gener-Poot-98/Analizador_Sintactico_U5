57636ce2f0221eb5df51d47bc6b99d5bbb8b95e1
Gener Emmanuel Poot Can <60999054+GenerEmmanuelPootCan@users.noreply.github.com>
2021-01-26 17:37:37 -0600

    Update analizador_sintactico.py

--- analizador_sintactico.py
+++ analizador_sintactico.py
@@ -1,9 +1,11 @@
+#Se importan las librerias necesarias
 import sys
 import ply.yacc as yacc
-from analizador_lexico import tokens
+from analizador_lexico import tokens #Se importan los tokens declarados en el analizador lexico.
 
 VERBOSE = 1
 
+#A continuación se declaran las precedencias.
 precedence = (
     ('left', 'INCLUDE', 'REQUIRE'),
     ('left', 'COMA'),
@@ -22,6 +24,10 @@ precedence = (
     ('right', 'PRIVATE', 'PROTECTED', 'PUBLIC'),
 )
 
+
+#A continuación se declaran varias funciones las cuales nos servirán para poder validar los errores sintacticos.
+#En este apartado se hace uso de los tokens que fueron declarados en el analizador lexico.
+
 def p_program(p):
     'program : declaration_list'
 			 
@@ -34,6 +40,7 @@ def p_declaration_list(p):
    '''
    pass
 
+#Se declara una funcion que nos servirá para juntas las posibles formas de declaración
 def p_declaration(p):
 	'''declaration : var_declaration
 				   | fun_declaration
@@ -49,6 +56,7 @@ def p_declaration(p):
 	'''
 	pass
 
+#Se declara una función que valida las distintas formas de escribir una expresión
 def p_echo_stmt(p):
 	'''echo_stmt : echo_stmt ECHO STRING PUNTOYCOMA
 				 | echo_stmt ECHO IDVAR PUNTOYCOMA
@@ -59,6 +67,7 @@ def p_echo_stmt(p):
 	'''
 	pass
 
+#Esta funcion es para validar la estructura que tendrá "Alert"
 def p_alert_stmt(p):
 	'''alert_stmt : ALERT PARENT_IZQ STRING PARENT_DER PUNTOYCOMA
 				  | empty
@@ -66,18 +75,21 @@ def p_alert_stmt(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar los encabezados
 def p_header_declaration(p):
 	'''header_declaration : REQUIRE PARENT_IZQ STRING PARENT_DER PUNTOYCOMA
                           | INCLUDE PARENT_IZQ STRING PARENT_DER PUNTOYCOMA
     '''
 	pass
 
+#Se declara una función que nos servirá para validar la declaración de una clase
 def p_class_declaration(p):
 	'''class_declaration : area CLASS VARIABLE LLAVE_IZQ attribute LLAVE_DER
 						 | CLASS VARIABLE LLAVE_IZQ attribute LLAVE_DER
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la forma en la que se ponen atributos
 def p_attribute1(p):
 	'''attribute : attribute area var_declaration
 				 | area var_declaration
@@ -86,7 +98,7 @@ def p_attribute1(p):
 	'''
 	pass
 
-
+#Se declara una funcioón que nos servirá para validar si es private, public o protected
 def p_area(p):
 	'''area : PRIVATE
 			| PUBLIC
@@ -94,6 +106,7 @@ def p_area(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar las distintas formas de declarar una variable
 def p_var_declaration(p):
 	'''var_declaration : VARIABLE PUNTOYCOMA var_declaration
                        | VAR VARIABLE IGUAL simple_expression
@@ -113,41 +126,47 @@ def p_var_declaration(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una función
 def p_fun_declaration(p):
 	'''fun_declaration : FUNCTION VARIABLE PARENT_IZQ params PARENT_DER
 					   | FUNCTION VARIABLE PARENT_IZQ params PARENT_DER compount_stmt
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar una lista de parametros o simplemente un espacio vacio
 def p_params(p):
 	'''params : param_list
 			  | empty
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una lista de parametros
 def p_param_list(p):
 	'''param_list : param_list COMA param_list
 				  | param
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de un parametro
 def p_param(p):
 	'''param : VARIABLE
              | VARIABLE CORCHETE_IZQ CORCHETE_DER PUNTOYCOMA
     '''
 	pass
 
-
+#Se declara una función que nos servirá para validar la estructura de una declaración compuesta
 def p_compount_stmt(p):
 	'compount_stmt : LLAVE_IZQ echo_stmt local_declarations echo_stmt statement_list echo_stmt LLAVE_DER'
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una declaración de forma local
 def p_local_declarations(p):
 	'''local_declarations : local_declarations var_declaration
 						  | empty
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una lista de declaraciones
 def p_statement_list(p):
 	'''statement_list : statement_list statement
 					  | empty
@@ -155,6 +174,7 @@ def p_statement_list(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una declaración
 def p_statement(p):
 	'''statement : expression_stmt
 				 | compount_stmt
@@ -169,6 +189,7 @@ def p_statement(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar las posibles expresiones que acompañarian a una declaración
 def p_expression_stmt(p):
 	'''expression_stmt : expression PUNTOYCOMA
 					   | additive_expression
@@ -176,7 +197,7 @@ def p_expression_stmt(p):
 	'''
 	pass
 
-
+#Se declara una función que nos servirá para validar la estructura de un IF
 def p_selection_stmt_1(p):
 	'''selection_stmt : IF PARENT_IZQ expression PARENT_DER statement
 					  | IF PARENT_IZQ expression PARENT_DER statement selection
@@ -184,12 +205,14 @@ def p_selection_stmt_1(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de un ELSE y ELSEIF
 def p_selection(p):
 	'''selection : ELSE statement
 				 | ELSEIF statement selection
 	 '''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de un SWITCH CASE
 def p_selection_stmt_2(p):
 	'''selection_stmt : SWITCH PARENT_IZQ var PARENT_DER statement
 					  | CASE NUMERO DOS_PUNTOS statement BREAK PUNTOYCOMA
@@ -198,24 +221,29 @@ def p_selection_stmt_2(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de un FOR
 def p_iteration_stmt_1(p):
 	'iteration_stmt : FOR PARENT_IZQ var_declaration PUNTOYCOMA expression PUNTOYCOMA additive_expression PARENT_DER statement '
 	pass
+
+#Se declara una función que nos servirá para validar la estructura de un WHILE
 def p_iteration_stmt_2(p):
 	'iteration_stmt : WHILE PARENT_IZQ expression PARENT_DER statement'
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de un DO WHILE
 def p_iteration_stmt_3(p):
 	'iteration_stmt : DO LLAVE_IZQ statement PUNTOYCOMA LLAVE_DER WHILE PARENT_IZQ expression PARENT_DER'
 	pass
 
-
+#Se declara una función que nos servirá para validar la estructura de un return
 def p_return_stmt(p):
 	'''return_stmt : RETURN PUNTOYCOMA
 				   | RETURN expression PUNTOYCOMA
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una expresión
 def p_expression(p):
 	'''expression : VARIABLE relop NUMERO
 				  | simple_expression
@@ -227,13 +255,14 @@ def p_expression(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una variable
 def p_var(p):
 	'''var : VARIABLE
 		   | VARIABLE CORCHETE_IZQ expression CORCHETE_DER
 	'''
 	pass
 
-
+#Se declara una función que nos servirá para validar la estructura de una expresión simple
 def p_simple_expression(p):
 	'''simple_expression : additive_expression relop additive_expression
 						 | additive_expression
@@ -241,6 +270,7 @@ def p_simple_expression(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar expresiones de comparación como mayor, menor, etc.
 def p_relop(p):
 	'''relop : MENOR
 			 | MENOR_IGUAL
@@ -252,6 +282,7 @@ def p_relop(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de una expresión aditiva, es decir un incremento
 def p_additive_expression(p):
 	'''additive_expression : additive_expression addop term
     					   | term
@@ -262,24 +293,28 @@ def p_additive_expression(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar operadores basicos como la suma y resta
 def p_addop(p):
 	'''addop : SUMA
 			 | RESTA
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar algunos operadores
 def p_term(p):
 	'''term : term mulop factor
 			| factor
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar operadores basicos como la multiplicación y la división
 def p_mulop(p):
 	'''mulop : MULTIPLICACION
 			 | DIVISION
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar los parentesis en las expresiones
 def p_factor(p):
 	'''factor : PARENT_IZQ expression PARENT_DER
 			  | var
@@ -289,6 +324,7 @@ def p_factor(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar ya sea args o VOID
 def p_args(p):
 	'''args : args_list
 			| empty
@@ -296,32 +332,38 @@ def p_args(p):
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar lista de args junto al punto y coma
 def p_args_list(p):
 	'''args_list : args_list COMA expression
 				 | expression
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar expresiones booleanas
 def p_boolean(p):
 	'''boolean : TRUE
 			   | FALSE
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar la forma en la que se declara una nueva clase
 def p_tclass(p):
 	'typeclass : VARIABLE IDVAR IGUAL NEW constructor PUNTOYCOMA'
 	pass
 
+#Se declara una función que nos servirá para validar la estructura de un constructor
 def p_costructor(p):
 	'''constructor : VARIABLE PARENT_IZQ PARENT_DER
 				   | VARIABLE PARENT_IZQ args PARENT_DER
 	'''
 	pass
 
+#Se declara una función que nos servirá para validar los espacios vacios
 def p_empty(p):
 	'empty :'
 	pass
 
+#Se declara una función que nos servirá para mostrar en consola los posibles errores detectados
 def p_error(p):
     if VERBOSE:
         if p is not None:
@@ -335,17 +377,19 @@ def p_error(p):
         raise Exception('syntax', 'error')
 
 
-
+#Se hace uso de la libreria yacc
 parser = yacc.yacc()
 
-script = 'prueba.js'
-scriptfile = open(script, 'r')
-scriptdata = scriptfile.read()
+script = 'prueba.js' #En esta linea se agrega el nombre del archivo .js que se va analizar
+scriptfile = open(script, 'r') #Con esta linea abrimos el archivo .js
+scriptdata = scriptfile.read() #Con esta linea leemos el archivo .js
 #print (scriptdata)
 
+#Finalmente se hace las impresiones correspondientes para mostrar si se detectaron errores o no en el programa analizado
 print (chr(27)+"[0;36m"+"INICIA ANALISIS SINTACTICO"+chr(27)+"[0m")
-parser.parse(scriptdata, tracking=False)
+parser.parse(scriptdata, tracking=False) #En esta parte se mostrarían los errores en caso de existir alguna
 print (chr(27)+"[0;36m"+"TERMINA ANALISIS SINTACTICO"+chr(27)+"[0m")
+#Se imprime los integrantes del equipi
 print("Programa elaborado por:"+"\n"
         +"<<Mis Oy Cristina de Jesus>>"+"\n"
         +"<<Poot Can Gener Emmanuel>>"+"\n"
